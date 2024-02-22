e8b5acb339073b2d50e764fc68931bbccf7b0b31
Erik Seldenthuis <44495844+erikseldenthuis@users.noreply.github.com>
2024-02-22 12:04:11 +0100

    fix dubbele quote

--- GitFlowCrashCourse/Program.cs
+++ GitFlowCrashCourse/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Once upon a time");
             Console.WriteLine("I was falling in love");
-            Console.WriteLine("Now I'm only falling apart);
+            Console.WriteLine("Now I'm only falling apart");
         }
     }
 }
